af4a1d8196f544a8dd84fd4afa33e2328a901264
JWBSD <114621468+JWBSD@users.noreply.github.com>
2024-07-10 13:32:53 -0600

    Add files via upload

---
+++ EnrollMe.ps1
@@ -0,0 +1,5 @@
+﻿$env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Script Get-WindowsAutopilotInfo -Force
+Get-WindowsAutopilotInfo.ps1 -online
+Start-Process ms-settings:workplace
